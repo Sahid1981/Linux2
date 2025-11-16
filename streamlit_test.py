a4a8737e0d6b786eb962059656294ccd5a9103b2
Sahid1981 <juha.jermalainen@gmail.com>
2025-11-16 22:23:15 +0200

    lopullinen streamlit

--- streamlit_test.py
+++ streamlit_test.py
@@ -1,10 +1,30 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+
+@st.cache_resource
+def get_data():
+    conn = st.connection('mysql', type='sql')
+    query = """
+    SELECT c.name AS genre, COUNT(f.film_id) AS film_count
+    FROM category c
+    JOIN film_category fc ON c.category_id = fc.category_id
+    JOIN film f ON fc.film_id = f.film_id
+    GROUP BY c.name
+    ORDER BY film_count DESC;
+    """
+    df = conn.query(query, ttl=600)
+    return df
+
 def main():
-	st.title("Sähkön hinta")
-	df = pd.read_csv("chart2.csv")
-	ff = px.scatter(df, x="category", y="Hinta")
-	st.plotly_chart(ff, use_container_width=True)
+    st.title("Sakila: Elokuvien määrä genreittäin")
+    st.write("Näytetään kuinka monta elokuvaa on per genre")
+    data = get_data()
+    df = pd.DataFrame(data)
+    st.dataframe(df)
+
+    fig = px.bar(df, x="genre", y="film_count", title="Elokuvien määrä per genre", color="genre")
+    st.plotly_chart(fig, use_container_width=True)
+
 if __name__ == "__main__":
-	main()
+    main()
